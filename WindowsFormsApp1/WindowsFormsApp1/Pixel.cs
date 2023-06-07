5bfa5b8c646a8c76b34418a1d5b506267d7638a8
ALOHADAWN <61286670+ALOHADAWN@users.noreply.github.com>
2023-06-07 16:14:58 +0500

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Pixel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Pixel
+    {
+        public Point Point { get; set; }
+        public Color Color { get; set; }
+    }
+}
